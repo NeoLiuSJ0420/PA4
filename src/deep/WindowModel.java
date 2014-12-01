0351e3d4b8a70d7b186f2a552954b291c4360430
Neo Liu <sl3350@nyu.edu>
2014-12-01 10:35:05 -0800

    Converted to matrix forms

--- src/deep/WindowModel.java
+++ src/deep/WindowModel.java
@@ -12,6 +12,7 @@ import java.text.*;
 public class WindowModel {
 
 	protected SimpleMatrix L, W, U,b1,b2;
+	protected SimpleMatrix dL,dW,dU,db1,db2;
 	//
 	public int windowSize,wordSize, hiddenSize;
 	public double learningRate;
@@ -32,24 +33,33 @@ public class WindowModel {
 		double init=Math.sqrt(6.0)/Math.sqrt(50*windowSize+hiddenSize);
 		W=SimpleMatrix.random(hiddenSize,windowSize*50,(-1)*init,init, new Random());
 		b1=new SimpleMatrix(hiddenSize,1);
-		U=SimpleMatrix.random(hiddenSize,5,(-1)*init,init, new Random());
+		U=SimpleMatrix.random(5,hiddenSize,(-1)*init,init, new Random());
 		b2=new SimpleMatrix(5,1);
 	}
 
 	private SimpleMatrix extractL(List<Datum> Data,int count,int[] num) {
 		SimpleMatrix ans=new SimpleMatrix(50*windowSize,1);
 		String word=Data.get(count).word.toLowerCase();
-		num[1]=FeatureFactory.wordToNum.get(word);
+		
+		if (!FeatureFactory.wordToNum.containsKey(word))
+			num[1]=0;
+		else num[1]=FeatureFactory.wordToNum.get(word);
 		
 		
 		if ((count==Data.size()-1)||(Data.get(count+1).word.toLowerCase().equals("-docstart-"))) 
 			num[2]=31;
+		else if (!FeatureFactory.wordToNum.containsKey(Data.get(count+1).word.toLowerCase()))
+			num[2]=0;
 		else
 			num[2]=FeatureFactory.wordToNum.get(Data.get(count+1).word.toLowerCase());
+		
 		if ((count==0)||(Data.get(count-1).word.toLowerCase().equals("-docstart-"))) 
 			num[0]=30;
+		else if (!FeatureFactory.wordToNum.containsKey(Data.get(count-1).word.toLowerCase()))
+			num[0]=0;
 		else
 			num[0]=FeatureFactory.wordToNum.get(Data.get(count-1).word.toLowerCase());
+		
 		for (int i=0;i<windowSize;i++) 
 			ans=ans.combine(i*50, 0, NER.allVecs.extractVector(false,num[i]));
 		return ans;
@@ -80,76 +90,41 @@ public class WindowModel {
 			System.out.println(step);
 			for (int count=0;count<data.size();count++) {
 				String word=data.get(count).word.toLowerCase();
-				int y=label[count];
+				SimpleMatrix y=new SimpleMatrix(5,1);//y,5*1
+				y.set(label[count],0,1);
 				if (word.equals("-docstart-")) continue;
-				if (count!=0)
-					if (!FeatureFactory.wordToNum.containsKey(data.get(count-1).word.toLowerCase())) 
-						if (!data.get(count-1).word.toLowerCase().equals("-docstart-"))
-							continue;
-				if (!FeatureFactory.wordToNum.containsKey(data.get(count).word.toLowerCase())) continue;
-				if (count!=data.size()-1)
-					if (!FeatureFactory.wordToNum.containsKey(data.get(count+1).word.toLowerCase())) 
-						if (!data.get(count+1).word.toLowerCase().equals("-docstart-"))
-							continue;
 				int[] num=new int[3];
 				L=extractL(data,count,num);
 				SimpleMatrix z=W.mult(L).plus(b1);//z,h*1
 				SimpleMatrix a=new SimpleMatrix(z);//a,h*1
 				for (int i=0;i<hiddenSize;i++)
 					a.set(i,0,Math.tanh(a.get(i,0)));
-				SimpleMatrix q=U.transpose().mult(a).plus(b2);//q,5*1
+				SimpleMatrix q=U.mult(a).plus(b2);//q,5*1
 				double sum=0;
 				for (int i=0;i<5;i++) sum+=Math.exp(q.get(i,0));
 				SimpleMatrix p=new SimpleMatrix(5,1);//p,5*1
 				for (int i=0;i<5;i++) p.set(i,0,Math.exp(q.get(i,0))/sum);
-				score+=Math.log(p.get(y,0));
 				
-				double[] delta1=new double[5];
-				for (int i=0;i<5;i++)
-					delta1[i]=(-1)*(isSame(i,y)-p.get(i));
-				//update U
-				for (int i=0;i<hiddenSize;i++)
-					for (int j=0;j<5;j++) {
-						double temp=U.get(i,j);
-						double gradient=delta1[j]*a.get(i)+lamda*temp;
-						U.set(i,j,temp-learningRate*gradient);
-					}
-				//update b2
-				for (int j=0;j<5;j++) {
-					double temp=b2.get(j,0);
-					double gradient=delta1[j];
-					b2.set(j,0,temp-learningRate*gradient);
+				//delta2 and delta1
+				SimpleMatrix delta2=p.minus(y);//delta2,5*1
+				SimpleMatrix delta1=new SimpleMatrix(hiddenSize,1);//delta1,h*1
+				for (int i=0;i<hiddenSize;i++) {
+					double temp=0;
+					for (int j=0;j<5;j++) 
+						temp+=delta2.get(j)*U.get(j,i)*(1-Math.pow(a.get(i), 2));
+					delta1.set(i,0,temp);
 				}
-				//update W
-				for (int k=0;k<hiddenSize;k++)
-					for (int j=0;j<50*windowSize;j++) {
-						double temp=W.get(k,j);
-						double gradient=0;
-						for (int i=0;i<5;i++) {
-							gradient+=delta1[i]*U.get(k,i)*(1-Math.pow(a.get(k,0),2))*L.get(j)+lamda*temp;
-						}
-						W.set(k,j,temp-learningRate*gradient);
-					}
-				//update b1
-				for (int k=0;k<hiddenSize;k++) {
-					double temp=b1.get(k,0);
-					double gradient=0;
-					for (int i=0;i<5;i++) {
-						gradient+=delta1[i]*U.get(k,i)*(1-Math.pow(a.get(k,0),2));
-					}
-					b1.set(k,0,temp-learningRate*gradient);
-				}
-				//update allVecs/L
-				for (int l=0;l<windowSize*50;l++) {
-					int row=l%windowSize;
-					int col=num[l/50];
-					double temp=NER.allVecs.get(row,col);
-					double gradient=0;
-					for (int i=0;i<5;i++) 
-						for (int j=0;j<hiddenSize;j++) {
-							gradient+=delta1[i]*U.get(j,i)*(1-Math.pow(a.get(j,0),2))*W.get(j,l);
-						}
-					NER.allVecs.set(row,col,temp-learningRate*gradient);
+				//calculate gradients
+				BackPropagation(delta2,delta1,a);
+				//update
+				U=U.minus(dU.scale(learningRate));
+				b2=b2.minus(db2.scale(learningRate));
+				W=W.minus(dW.scale(learningRate));
+				b1=b1.minus(db1.scale(learningRate));
+				for (int i=0;i<windowSize*50;i++) {
+					int row=i%50;
+					int col=num[i/50];
+					NER.allVecs.set(row,col,NER.allVecs.get(row,col)-learningRate*dL.get(i,0));
 				}
 			}//finish 1 iter of SGD
 	
@@ -161,6 +136,14 @@ public class WindowModel {
 	}
 
 	
+	private void BackPropagation(SimpleMatrix delta2, SimpleMatrix delta1, SimpleMatrix a) {
+		dU=delta2.mult(a.transpose()).plus(U.scale(lamda));
+		db2=new SimpleMatrix(delta2);
+		dW=delta1.mult(L.transpose()).plus(W.scale(lamda));
+		db1=new SimpleMatrix(delta1);
+		dL=W.transpose().mult(delta1);
+	}
+
 	public void test(List<Datum> testData) throws IOException{
 		int[] label=new int[testData.size()];
 		for (int i=0;i<testData.size();i++) {
@@ -175,22 +158,13 @@ public class WindowModel {
 			String word=testData.get(count).word.toLowerCase();
 			int y=label[count];
 			if (word.equals("-docstart-")) continue;
-			if (count!=0)
-				if (!FeatureFactory.wordToNum.containsKey(testData.get(count-1).word.toLowerCase())) 
-					if (!testData.get(count-1).word.toLowerCase().equals("-docstart-"))
-						continue;
-			if (!FeatureFactory.wordToNum.containsKey(testData.get(count).word.toLowerCase())) continue;
-			if (count!=testData.size()-1)
-				if (!FeatureFactory.wordToNum.containsKey(testData.get(count+1).word.toLowerCase())) 
-					if (!testData.get(count+1).word.toLowerCase().equals("-docstart-"))
-						continue;
 			int[] num=new int[3];
 			L=extractL(testData,count,num);
 			SimpleMatrix z=W.mult(L).plus(b1);//z,h*1
 			SimpleMatrix a=new SimpleMatrix(z);//a,h*1
 			for (int i=0;i<hiddenSize;i++)
 				a.set(i,0,Math.tanh(a.get(i,0)));
-			SimpleMatrix q=U.transpose().mult(a).plus(b2);//q,5*1
+			SimpleMatrix q=U.mult(a).plus(b2);//q,5*1
 			double sum=0;
 			for (int i=0;i<5;i++) sum+=Math.exp(q.get(i,0));
 			SimpleMatrix p=new SimpleMatrix(5,1);//p,5*1
